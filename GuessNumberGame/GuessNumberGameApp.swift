093d580cbc860e5cf0a4929ffe03d8f62b12a698
Tatiana Lazarenko <tan.strebkova@gmail.com>
2024-04-29 18:50:14 +0300

    Initial Commit

---
+++ GuessNumberGame/GuessNumberGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessNumberGameApp.swift
+//  GuessNumberGame
+//
+//  Created by Tatiana Lazarenko on 4/29/24.
+//
+
+import SwiftUI
+
+@main
+struct GuessNumberGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
